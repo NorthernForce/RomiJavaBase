0eebac40d37e1bfcf336e791260a69afa5187577
cml1010101 <cml1010101@gmail.com>
2022-10-11 19:20:24 -0400

    commit

--- src/main/java/frc/robot/commands/DefaultAutonomous.java
+++ src/main/java/frc/robot/commands/DefaultAutonomous.java
@@ -5,14 +5,12 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotContainer;
 import static frc.robot.RobotContainer.romiDrivetrain;
-import static frc.robot.subsystems.RomiDrivetrain.*;
 
 public class DefaultAutonomous extends CommandBase {
   PIDController controller;
+  volatile boolean enabled = false;
   /** Creates a new DefaultAutonomous. */
   public DefaultAutonomous() {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -28,11 +26,15 @@ public class DefaultAutonomous extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    romiDrivetrain.arcadeDrive(controller.calculate(romiDrivetrain.getLeftDistanceInch()), 0);
   }
   public void driveForward(double inches)
   {
     romiDrivetrain.resetEncoders();
-    controller = new PIDController(inches, inches, inches)
+    controller = new PIDController(1, 0, 0);
+    controller.setSetpoint(inches);
+    enabled = true;
+    while (enabled);
   }
   // Called once the command ends or is interrupted.
   @Override
