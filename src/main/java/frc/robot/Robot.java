146ba6eb813700496f4833bba196fdcd24b7ad0a
cml1010101 <cml1010101@gmail.com>
2022-10-11 20:40:30 -0400

    commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.DriveToDistance;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -57,7 +58,6 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
--- src/main/java/frc/robot/commands/DriveToDistance.java
+++ src/main/java/frc/robot/commands/DriveToDistance.java
@@ -8,33 +8,31 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import static frc.robot.RobotContainer.romiDrivetrain;
 
-public class DefaultAutonomous extends CommandBase {
-  PIDController controller;
-  volatile boolean enabled = false;
+public class DriveToDistance extends CommandBase {
+  private PIDController controller;
+  private volatile boolean enabled = false;
+  private double inches;
   /** Creates a new DefaultAutonomous. */
-  public DefaultAutonomous() {
+  public DriveToDistance(double inches) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(romiDrivetrain);
+    this.inches = inches;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    romiDrivetrain.resetEncoders();
+    controller = new PIDController(0.2, 0, 0);
+    controller.setSetpoint(inches);
+    controller.reset();
+    enabled = true;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    romiDrivetrain.arcadeDrive(controller.calculate(romiDrivetrain.getLeftDistanceInch()), 0);
-  }
-  public void driveForward(double inches)
-  {
-    romiDrivetrain.resetEncoders();
-    controller = new PIDController(1, 0, 0);
-    controller.setSetpoint(inches);
-    enabled = true;
-    while (enabled);
+    romiDrivetrain.arcadeDrive(controller.calculate(romiDrivetrain.getLeftDistanceInch()) * 0.3, 0);
   }
   // Called once the command ends or is interrupted.
   @Override
@@ -43,6 +41,6 @@ public class DefaultAutonomous extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return controller.atSetpoint();
   }
 }
